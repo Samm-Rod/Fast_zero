18a68c7a3631dbcd3b31e5b4dd7acc01cea5e758
samm-rod <smk191419@gmail.com>
2025-06-15 00:57:50 -0300

    Atualizando endpoint para usar o banco de dados real

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ola_mundo(client):
     response = client.get('/')
@@ -29,18 +31,18 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'email': 'alice@example.com',
-                'username': 'alice',
-            },
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -57,12 +59,8 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@gmail.com',
-        'id': 1,
-    }
+    assert response.json() == {'message': 'User deleted'}
